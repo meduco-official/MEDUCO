c4b25a1a67d6dd823e25ad1f1f9a524d7ce016ff
MEDUCO <karupaiyaa69@gmail.com>
2026-07-23 15:45:21 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded",()=>{
+
+const buttons=document.querySelectorAll("button");
+
+buttons.forEach(button=>{
+
+button.addEventListener("click",()=>{
+
+alert("Welcome to MEDUCO 🩺 Start your free medical learning journey!");
+
+});
+
+});
+
+});
